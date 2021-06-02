cdcbc02395e811902e3b5d4e924d24baa3dcbc56
Hassan97-js <hassansyr58@gmail.com>
2021-06-02 16:04:17 +0100

    Fixed a bug in /launch route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 import { HiddenNav, HiddenFooter } from "./utils/Routers";
 import WelcomeSpaceX from "./views/WelcomeSpaceX/WelcomeSpaceX";
@@ -9,7 +9,6 @@ import "./App.css";
 
 function App() {
   const [isVisited, setIsVisited] = useState(localStorage.getItem("visited"));
-
   return (
     <div className="App">
       <div className={isVisited && "content-wrap"}>
@@ -19,9 +18,7 @@ function App() {
             <Route exact path="/">
               {!isVisited ? <WelcomeSpaceX isVisited={isVisited} setIsVisited={setIsVisited} /> : <Redirect to="/launches" />}
             </Route>
-            <Route path="/launches">
-              <SpaceXLaunches />
-            </Route>
+            <Route path="/launches">{isVisited ? <SpaceXLaunches /> : <Redirect to="/" />}</Route>
             <Route path="/rockets"></Route>
             <Route path="/capsules"></Route>
           </Switch>
